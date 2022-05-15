7e927747ad5ffe329c84f43a1bdb8e1577794285
Croft-Ritete Shailo <scroftritete1@woolworths.com.au>
2022-05-15 22:57:14 +1000

    added final versions and updated README

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Calculator
+This is my calculator project.
+
+It follows the MVP and contains the following functions:
+
+- Addition. Output display should add values together
+
+* Subtraction. Output should minus values and also be able to display and calculate negative numbers.
+
+÷ Division. Output should be able to divide values.
+
+x Multiplication. Output should be able to multiply values entered from thee input.
+
+CE Clear All. When clicked all output display should return back to 0.
+
+This is not using the eval function. We are using divs to create a grid for the buttons. Buttons are made up of <input> sections which are manipulated with the 'onclick' event attribute that contains our JS functions.
+
+I have used different resources to achieve the result below and included detailed notes in the JS file.
+
+Css is basic but the color pallette follows the reference image used.
+
+This calculator gone through multiple evolutions and the code has been changed many times but this was the most sound solution.
--- index.js
+++ index.js
@@ -1,31 +1,128 @@
-const clearButton = document.getElementById("clear");
+// Set up the stack 
+let stack = []; //start stack off as an empty array
+let input = 0; //start index from zero
 
-console.log(clearButton);
+function calculateStack(value) //this function will calculate the input stack when clicking the equals button
+ {
+  if (input !== 0) //conditional- if input is not 0
+   { 
+    input = parseFloat(input); //turn the string into a float number
 
-const numbers = document.querySelectorAll(".number");
+    addToStack(input);//function from line 59, add the number to the stack
+  }
 
-console.log(numbers);
+// OPERATORS
+  let output = value[0]; //declare output as the value of value
+  let dividedByZero = 0; 
 
-const screen = document.getElementById("screen");
+  // start the loop
+  for (let i = 2; i < value.length; i = i + 2) 
 
-console.log(screen);
+  {
+    switch (stack[i - 1]) //
+    { 
+      // ADD
+      case "+":
+        output += value[i];
+        break;
+      // MINUS
+      case "-":
+        output -= value[i];
+        break;
+      // DIVIDE
+      case "÷":
+        if (value[i] === 0) dividedByZero = 1;
+        else output = output / value[i];
+        break;
+      // TIMES
+      case "x":
+        output = output * value[i];
+        break;
+    }
+  }
 
-console.log(screen.innerHTML);
+  // if these operators are switched around this will break the code. 
+  // //////////////////////////////
+  output = output.toFixed(10); //converts a number to a string
+  output = parseFloat(output); //converts string to a number
 
-clearButton.addEventListener("click", () => {
-  screen.innerHTML = "";
+  if (dividedByZero === 1) 
+  {
+    clearAll();
+    document.getElementById("output").innerHTML = "ERROR";
+  } else {
+    document.getElementById("output").innerHTML = output;
+    input = output;
+    stack = [];
+  }
+}
 
-  // numbers[0].innerHTML = 9;
 
-  //   for (let i = 0; i < numbers.length; i++) {
-  //     numbers[i].innerHTML = parseInt(numbers[i].innerHTML) + 1;
-  //   }
-});
+function addToStack(input) //this function adds the new input to the stack-used at the top
+{
+  stack.push(input);
+}
 
-const addEvent = (n) => {
-  n.addEventListener("click", () => {
-    screen.innerHTML += n.innerHTML;
-  });
-};
+// ----------------------------------
 
-numbers.forEach((n) => addEvent(n));
+function clearAll() //created a function to set innerHTML of output to "0" when clicked. see HTML line 19.
+ {
+  stack = [];
+  input = 0;
+  document.getElementById("output").innerHTML = "0";
+}
+
+// ----------------------------------
+
+function number(button) //Created a function for any input assigned "number" onclick. Connected to each numer input. 
+{ 
+  if (    
+    document.getElementById("output").innerHTML === "ERROR" ||
+    (document.getElementById("output").innerHTML == "0" &&
+      button != ".")
+  ) //Conditional: if output is equal to error or if output is equal to 0
+    // and button is not equal to "." 
+  
+  {
+    document.getElementById("output").innerHTML = "";
+  }
+    // then innerHTML for the output should display as blank. 
+
+  if (!(button === ".") || !input.match(/[.]/))
+    // Conditional: if the decimal button is not clicked(input of button pressed is not decimal)
+  {
+    input += button;
+    document.getElementById("output").innerHTML += button;
+    // then innerHTML should display the assignment of the clicked button 
+  }
+}
+
+// -------------------------------
+
+// function that takes in the input the user selects
+// this will determine the output on the display
+// function operate(button) this is manipulating our operator buttons
+{
+  if (input !== 0 && input !== "-") {
+    input = parseFloat(input);  // this will turn the string 
+                                // into  a floating number
+   
+    addToStack(input);
+    addToStack(button);  // adds the numeric input to the 
+                         // array || equation
+    document.getElementById("output").innerHTML += button;
+    input = 0;           // grab the display element 
+                         // change display innerHTML based off input selected 
+  }
+  // 
+  if (button == "-" && isNaN(stack[0]) && input !== "-") {
+    input = "-";         // if button is not a number
+                         // then return to the first element in the stack
+                         // and if input is equal to minus
+
+
+    document.getElementById("output").innerHTML = "-";
+  }
+}                        // change innerHTML of output 
+                         // to display minus
+                         
